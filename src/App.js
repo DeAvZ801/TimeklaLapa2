2641ffbab1c69ee13aae81eab7dba08083de56e8
DeAvZ801 <rsdqag@inbox.lv>
2024-04-16 14:07:55 +0300

    Next

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from './components/Galvene/Galvene'
 import Content from './components/content/Content'
 import Video from './components/RTUvid/Video'
 import Footer from './components/Kajiene/Kajiene'
-import Hero from './components/hero/Hero'
+import Next from './components/next/Next'
 import Information from './components/information/Information'
 import Columns from './components/columns/Columns'
 import Image from './components/image/Image'
@@ -13,7 +13,7 @@ function App() {
             <Header />
             <Video />
             <Content />
-            <Hero />
+            <Next/>
             <Information />
             <Columns />
             <Image />
---
+++ src/components/next/Next.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './next.css';
+import Burti from '../../assets/Burti.webp';
+import Vestibils from '../../assets/Vestibils.webp';
+import Kampuss from '../../assets/Kampuss.webp';
+
+function Next() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const images = [Burti, Vestibils, Kampuss];
+    
+    return (
+        <div className="hero-container">
+           
+            <div className="hero-gallery">
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image}
+                        alt={`Slide ${index}`}
+                        className={activeIndex === index ? "active" : ""}
+                        loading="lazy"
+                    />
+                ))}
+                <button
+                    className="next-button"
+                    onClick={() => setActiveIndex((activeIndex + 1) % images.length)}
+                >
+                    Nākamais
+                </button>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Next;
